6d63880c5855233cf7612cfe419d3671c2637eae
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-04-02 19:23:20 +0300

    RegisterUser

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,6 @@
 import LoginForm from "@/app/components/ui/LoginForm";
 
 const LoginPage = () => {
-  const logIn = async (formData: FormData) => {
-    "use server";
-
-    console.log("click");
-  };
-
   return (
     <section className="mt-4 flex flex-col items-center">
       <h3 className="text-2xl text-sky-900">Login Page</h3>
---
+++ app/register-user/page.tsx
@@ -0,0 +1,12 @@
+import RegisterUserForm from "./RegisterUserForm";
+
+const RegisterUserPage = () => {
+  return (
+    <section className="flex flex-col items-center m-4">
+      <h3 className="text-2xl text-sky-900">Register User Page</h3>
+      <RegisterUserForm />
+    </section>
+  );
+};
+
+export default RegisterUserPage;
