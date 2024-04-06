48655a422c6120a2fe4560442d841ff33416a5d2
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-04-06 20:03:51 +0300

    Typing

--- app/api/index.ts
+++ app/api/index.ts
@@ -5,10 +5,14 @@ const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
 });
 
-pool.query('SELECT NOW()', (err, res) => {
-  if (err) {
-    console.error('Error executing query', err.stack);
-  } else {
-    console.log('Connected successfully', res.rows[0]);
-  }
+interface QueryResult {
+    rows: any[];
+}
+
+pool.query('SELECT NOW()', (err: Error, res: QueryResult) => {
+    if (err) {
+        console.error('Error executing query', err.stack);
+    } else {
+        console.log('Connected successfully', res.rows[0]);
+    }
 });
