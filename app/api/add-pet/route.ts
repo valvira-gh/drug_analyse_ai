adf587154812889657c61edee05ac23f3b1be3c1
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-04-10 05:09:30 +0300

    Example works!

---
+++ app/api/add-pet/route.ts
@@ -0,0 +1,17 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const petName = "Vili";
+  const ownerName = "Valtteri";
+
+  try {
+    await sql`INSERT INTO Pets (Name, Owner) VALUES (${petName}, ${ownerName});`;
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+
+  const pets = await sql`SELECT * FROM Pets;`;
+  return NextResponse.json({ pets }, { status: 200 });
+}
